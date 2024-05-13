cd0ffc390ba423171f264d1fbdd7b80631a5bda3
vinit6515 <143284532+vinit6515@users.noreply.github.com>
2024-05-13 22:55:27 +0530

    Form Validation

--- FORMS/main.js
+++ FORMS/main.js
@@ -1,6 +1,15 @@
-function setError (id,error){// This function basically sends the error string to id
+function validateForm() {
+    var name = document.forms['myForm']["fname"].value;
 
-}
-function validateForm(){
+    if (name.trim() === "") {
+        setError("nameError", "Name cannot be empty");
+        return false;
+    }
 
+    console.log("Name:", name); // Log the value of the name field
+
+    // Add more validation logic for other fields here
+
+    return true;
 }
+
